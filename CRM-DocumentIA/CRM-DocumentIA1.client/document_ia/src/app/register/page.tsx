5e6397d4a431887afcc709e8c412b89c4ee81603
MarlonHerrera01 <marlon.1701914781@ucaldas.edu.co>
2025-09-15 11:49:56 -0500

    Creación de frontend en next.js y configuracion de tailwind ultima version

---
+++ CRM-DocumentIA/CRM-DocumentIA1.client/document_ia/src/app/register/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export default function Register() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">
+        Register
+      </h1>
+      <p>Contenido de la página register</p>
+    </div>
+  );
+}
